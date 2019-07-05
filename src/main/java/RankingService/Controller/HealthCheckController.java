69f9dd426a6c68317ff4af9f40bb2c1a5d85f89a
Abhinav <abhinav.mudgal@oyorooms.com>
2019-07-05 12:07:36 +0530

    health

--- src/main/java/RankingService/Controller/HealthCheckController.java
+++ src/main/java/RankingService/Controller/HealthCheckController.java
@@ -1,20 +1,18 @@
 package RankingService.Controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
 
-    @RequestMapping(value="/ping", method = RequestMethod.GET)
-    public String ping(){
-        LOGGER.info("Reciever ping");
-        return "OK";
+    @RequestMapping(value = "/ping")
+    @ResponseBody
+    public String healthCheck() {
+        return HttpStatus.OK.getReasonPhrase();
     }
-
 }
+

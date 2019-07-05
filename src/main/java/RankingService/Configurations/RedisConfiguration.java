756e01e52538cb8a0e2f107c6de05e5ded0582db
Abhinav <abhinav.mudgal@oyorooms.com>
2019-07-05 13:03:10 +0530

    Redis cluster config

--- src/main/java/RankingService/Configurations/RedisConfiguration.java
+++ src/main/java/RankingService/Configurations/RedisConfiguration.java
@@ -3,18 +3,24 @@ package RankingService.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Arrays;
+
 @Configuration
 public class RedisConfiguration {
     @Bean
     public JedisConnectionFactory connectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("oyoautocomplete-stag.h2xiyd.clustercfg.apse1.cache.amazonaws.com");
-        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(configuration);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        RedisNode node = new RedisNode("oyoautocomplete-stag.h2xiyd.clustercfg.apse1.cache.amazonaws.com", 6379)
+        clusterConfiguration.setClusterNodes(Arrays.asList(node));
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(clusterConfiguration);
         return connectionFactory;
     }
 
